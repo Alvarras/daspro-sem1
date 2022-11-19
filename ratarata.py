350699adb7cdb49f5483feea32efd471927cb979
Bakugo <varrasrivqi@gmail.com>
2022-11-19 11:10:35 +0700

    perubahan kedua

--- ratarata.py
+++ ratarata.py
@@ -4,8 +4,8 @@ sum = 0
 average = 0.0
 print("\nMasukan 10 bilangan : ")
 for i in range(0, 10):
- print("Data ke - ",i+1, ">")
- numbers[i]=int(input())
- sum+=numbers[i]
+    print("Data ke - ",i+1, ">")
+    numbers[i]=int(input())
+    sum+=numbers[i]
 average=float(sum)/count
 print("\n Nilai rata-ratanya adalah: ",average,"\n")
